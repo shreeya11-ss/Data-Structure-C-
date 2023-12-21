a633d9a70d02c83ad4099f520329d0b1f98fcd87
Shreeya Sharma <shreeyasharma13112004@gmail.com>
2023-12-21 20:16:21 +0530

    added insertion sort

---
+++ Sorting Algorithms/Insertion_sort.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int arr[]={45,90,23,56,12,6};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    for(int i=1;i<size;i++){
+        int key=arr[i];
+        int j;
+        for(j=i-1;j>=0 && arr[j]>key;j--){
+            arr[j+1]=arr[j];
+        }
+         arr[j+1]=key;
+   }
+   printf("Sorted array:");
+   for(int k=0;k<size;k++){
+    printf("%d\n",arr[k]);
+   }
+}
